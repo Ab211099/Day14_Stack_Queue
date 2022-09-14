b6512488a9c882f21bbe8fdb6e9b8ac24c4c6f82
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-14 19:16:56 +0530

    [Abhi]Added:Welcome program for Stack & Queue

---
+++ StackDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using StackDemo0;
+using System.Collections.Generic;
+
+namespace StackDemo;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Welcome to stack and Queue program");
+
+
+        }
+    }
